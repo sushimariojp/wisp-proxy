0f20f83a5c4e114a1a90eb42e118351e41f31957
osushixjp <187252633+sushimariojp@users.noreply.github.com>
2026-03-28 02:16:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const { server: wisp } = require('@mercuryworkshop/wisp-js/server');
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('wisp server is active');
@@ -10,7 +9,8 @@ server.on('upgrade', (req, socket, head) => {
     wisp.routeRequest(req, socket, head);
 });
 
-const PORT = 8080;
-server.listen(PORT, () => { 
-    console.log(`wisp server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    console.log(`wisp server is stareted`);
 });
